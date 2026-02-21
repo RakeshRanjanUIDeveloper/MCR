ceb2438b48fdd3d646d211d537a281eb64f6fc6e
Rakesh Ranjan <rakeshmodi034@gmail.com>
2026-02-21 23:14:42 +0530

    code added

--- namastedev-machinecoding-app/src/components/CopyClipboard.jsx
+++ namastedev-machinecoding-app/src/components/CopyClipboard.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const CopyClipboard = () => {
-    const handleCopy = () =>{
-        
+    const [enteredValue, setEnteredValue] = useState('');
+    const [showResult, setShowResult] = useState('')
+    const handleCopy = async () => {
+        try {
+            await navigator.clipboard.writeText(enteredValue);
+            setShowResult("Copied Successfully ✅");
+        } catch (error) {
+            setShowResult("Failed to copy ❌");
+        }
+        setTimeout(() => {
+            setShowResult("");
+        }, 2000);
     }
     return (
         <div className='copyToClipboard'>
@@ -11,11 +21,13 @@ const CopyClipboard = () => {
             <div className="copyToClipboard-container">
                 <div className='form'>
                     <label htmlFor='text'>Enter Your Text
-                        <input type='text' id="text" data-testid="input-field" placeholder='Type Something' />
+                        <input type='text' id="text" data-testid="input-field" placeholder='Type Something' value={enteredValue} onChange={(e) => setEnteredValue(e.target.value)} />
                     </label>
                     <button onClick={handleCopy} className='btn' data-testid="copy-button">Copy</button>
                 </div>
+
             </div>
+            <p>{showResult}</p>
         </div>
     )
 }
